3f0dc652fa21cc732539ace569f6c88de912df96
si-yao <siyao.li.szeyiu@gmail.com>
2015-04-15 15:38:15 -0400

    modified command line

--- Project3/src/main/java/Main.java
+++ Project3/src/main/java/Main.java
@@ -6,27 +6,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.print("Find Frequent Item Sets. Author: Siyao Li\n\n\nInstruction:\n\n" +
-                "Arguments: inputfile, outputfile, [delimiter=','], [threshold=0.7]\n" +
-                "You are required to at least provide the first two arguments, " +
-                "as delimiter is set ',' and threshold is set 0.7 by default.\n\n\n\n\n");
+                "Arguments: inputfile, outputfile, [min_supp=0.7], [min_conf=0.7]\n");
 
         if(args.length<2){
             throw new Error("Error: need at least two args: inputfile, outputfile!");
         }
         String inputfile = args[0];
         String outputfile = args[1];
-        String delimeter = null;
         double threshold = -1;
+        double conf = -1;
         if(args.length>2) {
-            delimeter = args[2];
-            threshold = Double.valueOf(args[3]);
-            if(threshold<=0||threshold>=1)
-                throw new Error("Error: threshold should be a value between 0 and 1!");
+            threshold = Double.valueOf(args[2]);
+            conf = Double.valueOf(args[3]);
         }
         FrequentItems fi = new FrequentItems(inputfile);
         fi.setOutputPath(outputfile);
-        if(delimeter!=null) fi.setDelimeter(delimeter);
-        if(threshold>0) fi.setThreshold(threshold);
+        if(threshold>=0 && threshold<=1) fi.setThreshold(threshold);
+        if(conf>=0 && conf<=1) fi.setMinConf(conf);
         fi.findFrequentPairs().findFrequentAll();
         fi.findRules();
         fi.printfile();
